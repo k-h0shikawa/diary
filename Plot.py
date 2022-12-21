e42dd71f503e022eff36a0186b521644744b4557
hoshikawa <hoshikawa1309@gmail.com>
2022-12-21 21:44:50 +0900

    add graph

--- Plot.py
+++ Plot.py
@@ -5,42 +5,11 @@ from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
  
 class Plot:
-    def __init__(self):
+    def __init__(self, x, y):
         """
         コンストラクタ
         """
-        # 適当にデータを生成
-        time_range = 30     # 30日分
-        dates = [datetime.datetime(2018,10,1) + datetime.timedelta(days=i) for i in range(time_range)]
-        vals = [np.sin(2 * np.pi * i / 240) for i in range(time_range)]
-
-        ax = plt.subplot()
-        ax.plot(dates, vals)
-
-        # Formatterでx軸の日付ラベルを月・日に設定
-        xfmt = mdates.DateFormatter("%m/%d")
-
-        # DayLocatorで間隔を日数に
-        xloc = mdates.DayLocator()
-
-
-        ax.xaxis.set_major_locator(xloc)
-        ax.xaxis.set_major_formatter(xfmt)
-
-        # x軸の範囲
-        ax.set_xlim(datetime.datetime(2018,10,1), datetime.datetime(2018,10,10)) 
-        ax.grid(True)
+        plt.plot(x, y);
         plt.show()
  
-    def analyze(self,text = None):
-        '''
-        感情分析
-        Returns:
-        --------
-            {'label':str,'score':float}  ネガポジの結果と確率を辞書形式で返す   
-       
-        '''
-        return self.nlp(self.document if text is None else text)
 
-if __name__ == "__main__":
-    plot = Plot()
--- TextToSentiment.py
+++ TextToSentiment.py
@@ -1,7 +1,23 @@
 from SentimentAnalysis import SentimentAnalysis
-
+from Plot import Plot
+import os
 
 if __name__ == "__main__":
     sentimentAnalysis = SentimentAnalysis()
-    sentimentAnalysis.read_file("./text/20221221.txt");
-    print(sentimentAnalysis.analyze())
+    filenames = os.listdir("./text");
+    sentiments = list();
+    days = list();
+    for filename in filenames:
+        sentimentAnalysis.read_file("./text/" + filename)
+        sentiment = sentimentAnalysis.analyze()[0]
+        
+        days.append(filename[:-4:])
+
+        if (sentiment["label"] == "ネガティブ"):
+            sentiments.append(-sentiment["score"])
+        else:
+            sentiments.append(sentiment["score"])
+    print(days)
+    print(sentiments)
+
+    Plot(days, sentiments)
